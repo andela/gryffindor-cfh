2ca8bd87eb6bf7cded91f89b27345fb7a0d6c6f1
Nnamso <andeladeveloper@Andelas-MacBook-Pro.local>
2017-08-23 16:54:35 +0100

    changed some http to https

--- test/game/game.js
+++ test/game/game.js
@@ -1,7 +1,7 @@
 var should = require('should');
 var io = require('socket.io-client');
 
-var socketURL = 'http://localhost:3000';
+var socketURL = 'https://localhost:3000';
 
 var options ={
   transports: ['websocket'],
