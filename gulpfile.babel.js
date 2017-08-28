580107c2f4d66e89bba47adaa41d1d51327c13dd
Celoka <eloka.chima@gmail.com>
2017-08-28 18:38:54 +0100

    Chore: Integrate Coveralls code coverage service (#16)
    * chore: Integrate Coverage code coverall service
    
    [Finishes #150438323]
    
    * chore: integrate coveralls code coverage service
    
    - remove coveralls repo token and add to travisci for privacy
    
    Finishes #150438323
    
    * chore: #150438323/coveralls-code-coverage
    
    - add gulp coverall to gulp file to run as 'gulp coveralls'
    - Rsolve merge conflit and rebase.
    
    Finishes #150438323
    
    * chore: 150438323 integarte coveralls code coverage service
    
    - Refactor gulp file to include gulp-istnabul and changed the test task
    
    Finishes #150438323
    
    * chore: 150438323 Integrate coveralls coverage code service
    
    - add .bowerrc
    -specify path for bootstrap file and angular files to fix front end bugs
    
    Finishes #150438323

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,14 +1,22 @@
 import gulp from 'gulp';
 import nodemon from 'gulp-nodemon';
 import mocha from 'gulp-mocha';
+<<<<<<< HEAD
 import rename from 'gulp-rename';
 import sass from 'gulp-sass';
+=======
+import concat from 'gulp-concat';
+import uglify from 'gulp-uglify';
+import rename from 'gulp-rename';
+import sass from 'gulp-ruby-sass';
+>>>>>>> Chore: Integrate Coveralls code coverage service (#16)
 import imagemin from 'gulp-imagemin';
 import cache from 'gulp-cache';
 import coveralls from 'gulp-coveralls';
 import exit from 'exit';
 import istanbul from 'gulp-istanbul';
 import bower from 'gulp-bower';
+<<<<<<< HEAD
 import babel from 'gulp-babel';
 import FileCache from 'gulp-file-cache';
 
@@ -43,6 +51,39 @@ gulp.task('watch', () => {
   gulp.watch('client/scss/**/*.scss', ['sass']);
   gulp.watch('client/img-assets/**/*', ['images']);
   gulp.watch(['src/config/env/**/*.json', 'src/app/views/**/*.jade'], ['copyServer']);
+=======
+
+
+// TODO: use browserify to concatinate js files 
+
+gulp.task('scripts', () => gulp.src(['public/js/**'])
+  .pipe(concat('main.js'))
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(uglify())
+  .pipe(gulp.dest('build/js')));
+
+gulp.task('sass', () => sass('public/scss/*.scss', { style: 'compressed' })
+  // .pipe(concat('main.css'))
+  // .pipe(rename({ suffix: '.min' }))
+  .pipe(gulp.dest('public/css')));
+
+gulp.task('images', () => gulp.src('public/img-assets/**/*')
+  .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
+  .pipe(gulp.dest('public/img')));
+
+gulp.task('server', () => {
+  nodemon({
+    script: 'server.js',
+    watch: ['server.js', 'app/**/*', 'config/**/*'],
+    ext: 'js'
+  });
+});
+
+gulp.task('watch', () => {
+  // gulp.watch('public/js/**/*.js', ['scripts']);
+  gulp.watch('public/scss/**/*.scss', ['sass']);
+  gulp.watch('public/img-assets/**/*', ['images']);
+>>>>>>> Chore: Integrate Coveralls code coverage service (#16)
 });
 
 gulp.task('coveralls', ['test'], () => gulp.src('./coverage/lcov.info')
@@ -50,7 +91,11 @@ gulp.task('coveralls', ['test'], () => gulp.src('./coverage/lcov.info')
   .pipe(exit()));
 
 
+<<<<<<< HEAD
 gulp.task('pre-test', () => gulp.src(['src/server.js', 'src/app/**/*.js', 'src/config/**/*.js'])
+=======
+gulp.task('pre-test', () => gulp.src(['server.js', 'app/**/*.js', 'config/**/*.js'])
+>>>>>>> Chore: Integrate Coveralls code coverage service (#16)
   // Covering files
   .pipe(istanbul())
   // Force `require` to return covered files
@@ -61,6 +106,7 @@ gulp.task('test', ['pre-test'], () =>
     .pipe(mocha({ reporter: 'spec' }))
     // Creating the reports after tests ran
     .pipe(istanbul.writeReports())
+<<<<<<< HEAD
     .once('error', () => {
       process.exit(1);
     })
@@ -96,3 +142,8 @@ gulp.task('copy', ['copyServer', 'copyClient']);
 gulp.task('bower', () => bower());
 gulp.task('default', ['sass', 'images', 'watch', 'server', 'bower', 'transpile', 'copy']);
 gulp.task('heroku:production', ['sass', 'images', 'bower', 'transpile', 'copy', 'compile']);
+=======
+);
+gulp.task('bower', () => bower());
+gulp.task('default', ['sass', 'images', 'watch', 'server', 'bower']);
+>>>>>>> Chore: Integrate Coveralls code coverage service (#16)
