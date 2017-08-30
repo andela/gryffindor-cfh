9df0051b9e98d69d01ebfd114387189957242815
yamen <osaretinosayamen@outlook.com>
2017-08-30 09:34:37 +0100

    refactor(passport.js): convert es5 code to es6
    #150438324

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,194 +1,189 @@
-import generateToken from '../config/generateToken';
-var mongoose = require('mongoose'),
-    LocalStrategy = require('passport-local').Strategy,
-    TwitterStrategy = require('passport-twitter').Strategy,
-    FacebookStrategy = require('passport-facebook').Strategy,
-    GitHubStrategy = require('passport-github').Strategy,
-    GoogleStrategy = require('passport-google-oauth').OAuth2Strategy,
-    User = mongoose.model('User'),
-    config = require('./config');
+import mongoose from 'mongoose';
+import { Stategy as LocalStrategy } from 'passport-local';
+import { Stategy as TwitterStrategy } from 'passport-twitter';
+import { Stategy as FacebookStrategy } from 'passport-facebook';
+import { Stategy as GitHubStrategy } from 'passport-github';
+import { Stategy as GoogleStrategy } from 'passport-google-oauth';
+import config from './config';
 
+const User = mongoose.model('User');
 
-module.exports = function(passport) {
-    //Serialize sessions
-    passport.serializeUser(function(user, done) {
-        done(null, user.id);
+export default (passport) => {
+// Serialize sessions
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findOne({
+      _id: id
+    }, (err, user) => {
+      user.email = null;
+      user.facebook = null;
+      user.hashed_password = null;
+      done(err, user);
     });
+  });
 
-    passport.deserializeUser(function(id, done) {
-        User.findOne({
-            _id: id
-        }, function(err, user) {
-            user.email = null;
-            user.facebook = null;
-            user.hashed_password = null;
-            done(err, user);
+  // Use local strategy
+  passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+  },
+  (emailArgument, password, done) => {
+    User.findOne({
+      email: emailArgument
+    }, (err, user) => {
+    // console.log('finds the user, resolves');
+      if (err) {
+        console.log(err);
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, {
+          message: 'Unknown user'
         });
+      }
+      if (!user.authenticate(password)) {
+        console.log('finds user but incorrect password');
+        return done(null, false, {
+          message: 'Invalid password'
+        });
+      }
+      // user.email = null;
+      // user.hashed_password = null;
+      done(null, user);
     });
+  }
+  ));
 
-    //Use local strategy
-    passport.use(new LocalStrategy({
-            usernameField: 'email',
-            passwordField: 'password'
-        },
-        function(email, password, done) {
-            User.findOne({
-                email: email
-            }, function(err, user) {
-                // console.log('finds the user, resolves');
-                if (err) {
-                    console.log(err);
-                    return done(err);
-                }
-                if (!user) {
-                    console.log('doesnt find the user you were looking for');
-                    return done(null, false, {
-                        message: 'Unknown user'
-                    });
-                }
-                if (!user.authenticate(password)) {
-                    console.log('finds user but incorrect password');
-                    return done(null, false, {
-                        message: 'Invalid password'
-                    });
-                }
-                console.log('the user: ', user);
-                user.email = null;
-                user.hashed_password = null;
-                user.token = generateToken(user);
-                console.log('generates token');
-                return done(null, user);
-            });
-        }
-    ));
+  // Use twitter strategy
+  passport.use(new TwitterStrategy({
+    consumerKey: process.env.TWITTER_CONSUMER_KEY || config.twitter.clientID,
+    consumerSecret: process.env.TWITTER_CONSUMER_SECRET || config.twitter.clientSecret,
+    callbackURL: config.twitter.callbackURL
+  },
+  (token, tokenSecret, profile, done) => {
+    User.findOne({
+      'twitter.id_str': profile.id
+    }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        user = new User({
+          name: profile.displayName,
+          username: profile.username,
+          provider: 'twitter',
+          twitter: profile._json
+        });
+        user.save((err) => {
+          if (err) console.log(err);
+          return done(err, user);
+        });
+      } else {
+        return done(err, user);
+      }
+    });
+  }
+  ));
 
-    //Use twitter strategy
-    passport.use(new TwitterStrategy({
-            consumerKey: process.env.TWITTER_CONSUMER_KEY || config.twitter.clientID,
-            consumerSecret: process.env.TWITTER_CONSUMER_SECRET || config.twitter.clientSecret,
-            callbackURL: config.twitter.callbackURL
-        },
-        function(token, tokenSecret, profile, done) {
-            User.findOne({
-                'twitter.id_str': profile.id
-            }, function(err, user) {
-                if (err) {
-                    return done(err);
-                }
-                if (!user) {
-                    user = new User({
-                        name: profile.displayName,
-                        username: profile.username,
-                        provider: 'twitter',
-                        twitter: profile._json
-                    });
-                    user.save(function(err) {
-                        if (err) console.log(err);
-                        return done(err, user);
-                    });
-                } else {
-                    return done(err, user);
-                }
-            });
-        }
-    ));
+  // Use facebook strategy
+  passport.use(new FacebookStrategy({
+    clientID: process.env.FB_CLIENT_ID || config.facebook.clientID,
+    clientSecret: process.env.FB_CLIENT_SECRET || config.facebook.clientSecret,
+    callbackURL: config.facebook.callbackURL
+  },
+  (accessToken, refreshToken, profile, done) => {
+    User.findOne({
+      'facebook.id': profile.id
+    }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        console.log(profile);
+        user = new User({
+          name: profile.displayName,
+          email: (profile.emails && profile.emails[0].value) || '',
+          username: profile.username,
+          provider: 'facebook',
+          facebook: profile._json
+        });
+        user.save((err) => {
+          if (err) console.log(err);
+          user.facebook = null;
+          return done(err, user);
+        });
+      } else {
+        user.facebook = null;
+        return done(err, user);
+      }
+    });
+  }
+  ));
 
-    //Use facebook strategy
-    passport.use(new FacebookStrategy({
-            clientID: process.env.FB_CLIENT_ID || config.facebook.clientID,
-            clientSecret: process.env.FB_CLIENT_SECRET || config.facebook.clientSecret,
-            callbackURL: config.facebook.callbackURL
-        },
-        function(accessToken, refreshToken, profile, done) {
-            User.findOne({
-                'facebook.id': profile.id
-            }, function(err, user) {
-                if (err) {
-                    return done(err);
-                }
-                if (!user) {
-                    console.log(profile);
-                    user = new User({
-                        name: profile.displayName,
-                        email: (profile.emails && profile.emails[0].value) || '',
-                        username: profile.username,
-                        provider: 'facebook',
-                        facebook: profile._json
-                    });
-                    user.save(function(err) {
-                        if (err) console.log(err);
-                        user.facebook = null;
-                        return done(err, user);
-                    });
-                } else {
-                    user.facebook = null;
-                    return done(err, user);
-                }
-            });
-        }
-    ));
+  // Use github strategy
+  passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID || config.github.clientID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET || config.github.clientSecret,
+    callbackURL: config.github.callbackURL
+  },
+  (accessToken, refreshToken, profile, done) => {
+    User.findOne({
+      'github.id': profile.id
+    }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        user = new User({
+          name: profile.displayName,
+          email: profile.emails[0].value,
+          username: profile.username,
+          provider: 'github',
+          github: profile._json
+        });
+        user.save((err) => {
+          if (err) console.log(err);
+          return done(err, user);
+        });
+      } else {
+        return done(err, user);
+      }
+    });
+  }
+  ));
 
-    //Use github strategy
-    passport.use(new GitHubStrategy({
-            clientID: process.env.GITHUB_CLIENT_ID || config.github.clientID,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET || config.github.clientSecret,
-            callbackURL: config.github.callbackURL
-        },
-        function(accessToken, refreshToken, profile, done) {
-            User.findOne({
-                'github.id': profile.id
-            }, function(err, user) {
-                if (err) {
-                    return done(err);
-                }
-                if (!user) {
-                    user = new User({
-                        name: profile.displayName,
-                        email: profile.emails[0].value,
-                        username: profile.username,
-                        provider: 'github',
-                        github: profile._json
-                    });
-                    user.save(function(err) {
-                        if (err) console.log(err);
-                        return done(err, user);
-                    });
-                } else {
-                    return done(err, user);
-                }
-            });
-        }
-    ));
-
-    //Use google strategy
-    passport.use(new GoogleStrategy({
-            clientID: process.env.GOOGLE_CLIENT_ID || config.google.clientID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET || config.google.clientSecret,
-            callbackURL: config.google.callbackURL
-        },
-        function(accessToken, refreshToken, profile, done) {
-            User.findOne({
-                'google.id': profile.id
-            }, function(err, user) {
-                if (err) {
-                    return done(err);
-                }
-                if (!user) {
-                    user = new User({
-                        name: profile.displayName,
-                        email: profile.emails[0].value,
-                        username: profile.username,
-                        provider: 'google',
-                        google: profile._json
-                    });
-                    user.save(function(err) {
-                        if (err) console.log(err);
-                        return done(err, user);
-                    });
-                } else {
-                    return done(err, user);
-                }
-            });
-        }
-    ));
+  //  Use google strategy
+  passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID || config.google.clientID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET || config.google.clientSecret,
+    callbackURL: config.google.callbackURL
+  },
+  (accessToken, refreshToken, profile, done) => {
+    User.findOne({
+      'google.id': profile.id
+    }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        user = new User({
+          name: profile.displayName,
+          email: profile.emails[0].value,
+          username: profile.username,
+          provider: 'google',
+          google: profile._json
+        });
+        user.save((err) => {
+          if (err) console.log(err);
+          return done(err, user);
+        });
+      } else {
+        return done(err, user);
+      }
+    });
+  }
+  ));
 };
