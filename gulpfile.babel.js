bdd597b172ea655b6a470047d6bf5c21ea1dc9c0
Ibrahim Abdulazeez <ibrahim.abdulazeez@andela.com>
2017-09-01 16:15:03 +0100

    Chore/#150553071/gulp transpiler (#21)
    [Finishes #150553071]

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,7 +2,6 @@ import gulp from 'gulp';
 import nodemon from 'gulp-nodemon';
 import mocha from 'gulp-mocha';
 import concat from 'gulp-concat';
-import uglify from 'gulp-uglify';
 import rename from 'gulp-rename';
 import sass from 'gulp-ruby-sass';
 import imagemin from 'gulp-imagemin';
@@ -11,37 +10,39 @@ import coveralls from 'gulp-coveralls';
 import exit from 'exit';
 import istanbul from 'gulp-istanbul';
 import bower from 'gulp-bower';
+import babel from 'gulp-babel';
+import FileCache from 'gulp-file-cache';
 
+const fileCache = new FileCache();
 
-// TODO: use browserify to concatinate js files 
-
-gulp.task('scripts', () => gulp.src(['public/js/**'])
-  .pipe(concat('main.js'))
+gulp.task('sass', () => sass('client/scss/main.scss', { style: 'compressed' })
+  .pipe(concat('main.css'))
   .pipe(rename({ suffix: '.min' }))
-  .pipe(uglify())
-  .pipe(gulp.dest('build/js')));
-
-gulp.task('sass', () => sass('public/scss/*.scss', { style: 'compressed' })
-  // .pipe(concat('main.css'))
-  // .pipe(rename({ suffix: '.min' }))
   .pipe(gulp.dest('public/css')));
 
-gulp.task('images', () => gulp.src('public/img-assets/**/*')
-  .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
+gulp.task('images', () => gulp.src('client/img-assets/**/*')
+  .pipe(cache(imagemin({ optimizationLegivel: 5, progressive: true, interlaced: true })))
   .pipe(gulp.dest('public/img')));
 
-gulp.task('server', () => {
+gulp.task('server', ['compile'], () =>
   nodemon({
-    script: 'server.js',
-    watch: ['server.js', 'app/**/*', 'config/**/*'],
-    ext: 'js'
-  });
-});
+    script: 'dist/server.js',
+    watch: 'server',
+    tasks: ['compile', 'copyServer']
+  })
+);
+gulp.task('compile', () =>
+  gulp.src('src/**/*.js')
+    .pipe(babel())
+    .pipe(fileCache.cache())
+    .pipe(gulp.dest('./dist'))
+);
 
 gulp.task('watch', () => {
-  // gulp.watch('public/js/**/*.js', ['scripts']);
-  gulp.watch('public/scss/**/*.scss', ['sass']);
-  gulp.watch('public/img-assets/**/*', ['images']);
+  gulp.watch('client/js/**/*.js', ['transpile']);
+  gulp.watch('client/scss/**/*.scss', ['sass']);
+  gulp.watch('client/img-assets/**/*', ['images']);
+  gulp.watch(['src/config/env/**/*.json', 'src/app/views/**/*.jade'], ['copyServer']);
 });
 
 gulp.task('coveralls', ['test'], () => gulp.src('./coverage/lcov.info')
@@ -49,7 +50,7 @@ gulp.task('coveralls', ['test'], () => gulp.src('./coverage/lcov.info')
   .pipe(exit()));
 
 
-gulp.task('pre-test', () => gulp.src(['server.js', 'app/**/*.js', 'config/**/*.js'])
+gulp.task('pre-test', () => gulp.src(['src/server.js', 'src/app/**/*.js', 'src/config/**/*.js'])
   // Covering files
   .pipe(istanbul())
   // Force `require` to return covered files
@@ -67,5 +68,31 @@ gulp.task('test', ['pre-test'], () =>
       process.exit();
     })
 );
+gulp.task('transpile', () => gulp.src(['client/js/**/*.js'])
+  .pipe(babel())
+  .pipe(gulp.dest('public/js')));
+
+
+gulp.task('copyClient', () => {
+  // copy misc client files
+  gulp.src(['client/css/**'])
+    .pipe(gulp.dest('public/css'));
+  gulp.src(['client/views/**'])
+    .pipe(gulp.dest('public/views'));
+  gulp.src(['client/*.txt'])
+    .pipe(gulp.dest('public'));
+});
+
+gulp.task('copyServer', () => {
+  // copy misc server files
+  gulp.src(['src/app/views/**/*.jade'])
+    .pipe(gulp.dest('dist/app/views'));
+  gulp.src(['src/config/env/**/*.json'])
+    .pipe(gulp.dest('dist/config/env'));
+});
+
+gulp.task('copy', ['copyServer', 'copyClient']);
+
 gulp.task('bower', () => bower());
-gulp.task('default', ['sass', 'images', 'watch', 'server', 'bower']);
+gulp.task('default', ['sass', 'images', 'watch', 'server', 'bower', 'transpile', 'copy']);
+
