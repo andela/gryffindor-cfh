70f81be54a828929cab7f1effbb59495ab52961d
yamen <osaretinosayamen@outlook.com>
2017-09-05 18:46:25 +0100

    feature(auth.js): redirect after login
    Finishes #150438324

--- client/js/services/auth.js
+++ client/js/services/auth.js
@@ -1,28 +1,31 @@
 /* global angular */
 angular.module('mean.system')
-  .factory('AuthenticationService', ['$http', $http => ({
-    login: (userEmail, userPassword) => {
-      $http.post('/api/auth/login', {
-        email: userEmail,
-        password: userPassword
-      })
-        .then((result) => {
+  .factory('AuthenticationService', ['$http', '$location', '$window',
+    ($http, $location, $window) => ({
+      login: (userEmail, userPassword) => {
+        $http.post('/api/auth/login', {
+          email: userEmail,
+          password: userPassword
+        })
+          .then((result) => {
+            $location.path('/#!');
+            $window.location.reload();
           console.log(result); // eslint-disable-line
-        }, (error) => {
+          }, (error) => {
           console.log(error); // eslint-disable-line
-        });
-    },
-    signUp: (userName, userEmail, userPassword) => {
-      $http.post('/api/auth/signup', {
-        name: userName,
-        email: userEmail,
-        password: userPassword
-      })
-        .then((result) => {
+          });
+      },
+      signUp: (userName, userEmail, userPassword) => {
+        $http.post('/api/auth/signup', {
+          name: userName,
+          email: userEmail,
+          password: userPassword
+        })
+          .then((result) => {
           console.log(result);  // eslint-disable-line
-        }, (error) => {
+          }, (error) => {
           console.log(error); // eslint-disable-line
-        });
+          });
+      }
     }
-  }
-  )]);
+    )]);
