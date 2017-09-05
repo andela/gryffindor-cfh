670ac5e9dd1267cf4f0523aa8f257acdaa275dbb
Nnamso <andeladeveloper@Andelas-MacBook-Pro.local>
2017-09-05 21:23:57 +0100

    functionality(increase players): Increase maximum game players
    Fix API endpoints
    
    Start #150438326

--- client/js/services/userSearch.js
+++ client/js/services/userSearch.js
@@ -1,14 +1,28 @@
 angular.module('mean.system')
-  .factory('userSearch', ['$http', $http => ({
+  .factory('userSearch', ['$http', '$q', ($http, $q) => ({
     search(selectedUser) {
-      return $http.get(`/api/search/users/${selectedUser}`);
+      const deffered = $q.defer();
+      $http.get(`/api/search/users/${selectedUser}`)
+        .then((response) => {
+          deffered.resolve({ data: response.data });
+        }, (response) => {
+          deffered.reject(response);
+        });
+      return deffered.promise;
     },
 
-    sendInvite(mailObject) {
-      return $http.post('/api/users/emailInvite', {
+    sendInvite: (mailObject) => {
+      const deffered = $q.defer();
+      $http.post('/api/users/emailInvite', {
         To: mailObject.To,
         Link: mailObject.Link
-      });
+      })
+        .then((response) => {
+          deffered.resolve({ data: response.data });
+        }, (response) => {
+          deffered.reject(response);
+        });
+      return deffered.promise;
     }
 
   }
