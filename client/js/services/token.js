215834ab9507e2d0d4a10a52ba58d79abd443a59
yamen <osaretinosayamen@outlook.com>
2017-09-15 13:56:07 +0100

    feauture(token.js):
    `` modify token service to save user strings correctly ``
    
    Finishes #150438336

--- client/js/services/token.js
+++ client/js/services/token.js
@@ -8,14 +8,26 @@ angular
     getToken: () => (
       $window.localStorage.getItem('token')
     ),
-    clearToken: () => {
-      $window.localStorage.removeItem('token');
-    },
-    saveUser: (user) => {
-      $window.localStorage.setItem('user', user);
-    },
-    clearUser: () => {
-      $window.localStorage.removeItem('user');
+    clearToken: () => (
+      $window.localStorage.removeItem('token')
+    ),
+    saveEmail: email => (
+      $window.localStorage.setItem('email', email)
+    ),
+    saveUsername: username => (
+      $window.localStorage.setItem('username', username)
+    ),
+    getEmail: () => (
+      $window.localStorage.getItem('email')
+    ),
+    getUsername: () => (
+      $window.localStorage.getItem('username')
+    ),
+    clearEmail: () => (
+      $window.localStorage.removeItem('email')
+    ),
+    clearUsername: () => {
+      $window.localStorage.removeItem('username');
     }
   })
   ]);
