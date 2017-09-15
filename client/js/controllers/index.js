aec9f537cdf43616b0ef87b67323ff0efd8b8c6c
yamen <osaretinosayamen@outlook.com>
2017-09-15 13:52:12 +0100

    feature(index.js):
    `` listen for changes in requests using game service ``
    
    Finishes #150438336

--- client/js/controllers/index.js
+++ client/js/controllers/index.js
@@ -2,18 +2,23 @@
 angular.module('mean.system')
   .controller('IndexController',
     ['$scope', 'Global', '$location', 'socket', 'game', 'AvatarService', 'AuthService',
-      'LocalStorageService',
-    ($scope, Global, $location, socket, game, AvatarService, AuthService, TokenService) => { //eslint-disable-line
+      'LocalStorageService', 'userSearch',
+    ($scope, Global, $location, socket, game, AvatarService, AuthService, TokenService, userSearch) => { //eslint-disable-line
         $scope.global = Global;
         $scope.email = '';
         $scope.username = '';
         $scope.password = '';
+        $scope.friendRequests = [];
+        $scope.gameRequests = [];
+        $scope.resolvedFriendRequests = [];
+        $scope.friendNotifications = 0;
 
         $scope.showOptions = () => !AuthService.isAuthenticated();
 
         $scope.signOut = () => {
           TokenService.clearToken();
-          TokenService.clearUser();
+          TokenService.clearUsername();
+          TokenService.clearEmail();
           $location.path('#!/');
         };
 
@@ -21,8 +26,10 @@ angular.module('mean.system')
           if (isValid) {
             AuthService.login($scope.email, $scope.password)
               .then(({ data: { token, user } }) => {
+                const { username, email } = user;
                 AuthService.saveToken(token);
-                TokenService.saveUser(user);
+                TokenService.saveUsername(username);
+                TokenService.saveEmail(email);
                 $location.path('/#!');
               })
               .catch(() => {
@@ -34,8 +41,10 @@ angular.module('mean.system')
           if (isValid) {
             AuthService.signUp($scope.username, $scope.email, $scope.password)
               .then(({ data: { token, user } }) => {
+                const { username, email } = user;
                 AuthService.saveToken(token);
-                TokenService.saveUser(user);
+                TokenService.saveUsername(username);
+                TokenService.saveEmail(email);
                 $location.path('/#!');
               })
               .catch(() => {
@@ -43,6 +52,40 @@ angular.module('mean.system')
               });
           }
         };
+
+        const email = TokenService.getEmail();
+        const myUsername = TokenService.getUsername();
+
+        $scope.resolveFriendRequest = (inviterEmail, username, status) => {
+          $scope.resolvedFriendRequests.push(inviterEmail);
+          game.resolveFriendRequest(inviterEmail, username, email, myUsername, status);
+        };
+
+        $scope.checkRequestResolved = requestEmail => (
+          $scope.resolvedFriendRequests.indexOf(requestEmail) !== -1
+        );
+
+        const incrementNotifications = ({ inviter, inviterEmail }) => {
+          $scope.friendNotifications += 1;
+          $scope.friendRequests.push({ username: inviter, email: inviterEmail });
+        };
+
+        const incrementGameRequests = ({ username, link }) => {
+          $scope.friendNotifications += 1;
+          $scope.gameRequests.push({ username, link });
+        };
+
+        $scope.getRequests = () => {
+          userSearch.getRequests(email)
+            .then((data) => {
+              $scope.friendRequests = data.data;
+              $scope.friendNotifications = data.data.length;
+            })
+            .catch(error => (error));
+        };
+
+        game.getRequests(email, incrementNotifications);
+        game.getGameRequests(email, incrementGameRequests);
         $scope.avatars = [];
         AvatarService.getAvatars()
           .then((data) => {
